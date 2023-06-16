9a2528133ca3300d61b2ab7bda0ddc83d3b63124
Aman Jha <amanjha171@gmail.com>
2023-06-16 16:43:42 +0530

    second change

--- reactaman/src/App.js
+++ reactaman/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react'
-// import Resturant from "./components/Basics/Resturant";
-// import UseState from "./components/Hooks/useState";
-// import UseEffect from "./components/Hooks/useEffect";
-// import UseReducer from "./components/Hooks/useReducer";
-// import Todo from "./components/todoreact/todo"
-import Temp from "./components/Weather/temp";
+
+import Todo from "./components/todoreact/todo"
+
 const App = () => {
   return (
 
     <>
-    <Temp/>
-    {/* <Todo/> */}
-    {/* <UseReducer/> */}
-    {/* <UseEffect/> */}
-    {/* <UseState/> */} 
-      {/* <Resturant/> */}
+  
+    <Todo/>
+   
 
     </>
   )
